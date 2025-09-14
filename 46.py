0c5b7d658c0281afb91b32a21d53f5e175ed36b8
ricardocassio07 <ricardocassio271@gmail.com>
2025-09-14 16:31:57 -0300

    Aprendendo Python

---
+++ 46.py
@@ -0,0 +1,7 @@
+# 46. Escreva um programa que compara duas strings.
+s1 = (input("Digite algo: "))
+s2 = (input("Digite algo novamente: "))
+if (s1 != s2):
+    print("Você digitou duas coisas diferentes!")
+else:
+    print("Você digitou duas coisas iguais!")
