30a486740b04b2b5f1ea8c2bed5434c8c06ad171
ricardocassio07 <ricardocassio271@gmail.com>
2025-09-21 16:06:37 -0300

    Aprendendo Python

---
+++ 56.py
@@ -0,0 +1,82 @@
+# 6. Implemente um gerador de senhas seguras com diferentes critérios e validação.
+import random
+letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "J", "K", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+caracteresEspeciais = ["@", "!", "$", "#"]
+senha = []
+desejoValido = False
+while (desejoValido == False):
+    try:
+        desejoDoUsuario = (int(input("Você deseja que a senha contenha:\nDigite:\n1- Apenas números;\n2- Apenas letras;\n3- Números e letras;\n4- Números, letras e carateres especiais(! @ # $ % )\n-> ")))
+        if (desejoDoUsuario == 1):
+            i = 0
+            while (i < 8):
+                s = random.randint(0, 9)
+                numero = numeros[s]
+                numero = str(numero)
+                senha.append(numero)
+                i+=1
+            print("{}{}{}{}{}{}{}{}".format(senha[0], senha[1], senha[2], senha[3], senha[4], senha[5], senha[6], senha[7]))
+            desejoValido = True
+        elif (desejoDoUsuario == 2):
+            i = 0
+            while (i < 8):
+                s = random.randint(0, 25)
+                letra = letras[s]
+                maisculo = random.randint(0, 1)
+                if  (maisculo == 1):
+                    letra = letra.upper()
+                senha.append(letra)
+                i+=1
+            print("{}{}{}{}{}{}{}{}".format(senha[0], senha[1], senha[2], senha[3], senha[4], senha[5], senha[6], senha[7]))
+            desejoValido = True
+            desejoValido = True
+        elif (desejoDoUsuario == 3):
+            i = 0
+            while (i < 8):
+                letraOuNumero = random.randint(0, 1)
+                if (letraOuNumero == 0):
+                    s = random.randint(0, 25)
+                    letra = letras[s]
+                    maisculo = random.randint(0, 1)
+                    if  (maisculo == 1):
+                        letra = letra.upper()
+                    senha.append(letra)
+                    i+=1
+                else:
+                    s = random.randint(0, 9)
+                    numero = numeros[s]
+                    numero = str(numero)
+                    senha.append(numero)
+                    i+=1
+            print("{}{}{}{}{}{}{}{}".format(senha[0], senha[1], senha[2], senha[3], senha[4], senha[5], senha[6], senha[7]))
+            desejoValido = True
+        elif (desejoDoUsuario == 4):
+            i = 0
+            while (i < 8):
+                letraOuNumero = random.randint(0, 2)
+                if (letraOuNumero == 0):
+                    s = random.randint(0, 25)
+                    letra = letras[s]
+                    maisculo = random.randint(0, 1)
+                    if  (maisculo == 1):
+                        letra = letra.upper()
+                    senha.append(letra)
+                    i+=1
+                elif (letraOuNumero == 1):
+                    s = random.randint(0, 9)
+                    numero = numeros[s]
+                    numero = str(numero)
+                    senha.append(numero)
+                    i+=1
+                else:
+                    s = random.randint(0, 3)
+                    caractere = caracteresEspeciais[s]
+                    senha.append(caractere)
+                    i+=1
+            print("{}{}{}{}{}{}{}{}".format(senha[0], senha[1], senha[2], senha[3], senha[4], senha[5], senha[6], senha[7]))
+            desejoValido = True
+        else:
+            print("Digite apenas 1, 2, 3 ou 4!!!")
+    except ValueError:
+        print("Digite apenas números!!!")
---
+++ 57.py
@@ -0,0 +1,141 @@
+# 7. Crie um programa que simule um jogo da velha com validação de jogadas e detecção de vitória.
+import random
+A1 = "a1"
+A2 = "a2"
+A3 = "a3"
+B1 = "b1"
+B2 = "b2"
+B3 = "b3"
+C1 = "c1"
+C2 = "c2"
+C3 = "c3"
+jogoFinalizado = False
+rodada = 1
+jogadorDaVez = ""
+while (jogoFinalizado == False):
+    print("LINHAS - 'a', 'b' OU 'c'")
+    print("COLUNAS - '1', '2' OU '3'")
+    print("     1    2    3 ")
+    print("a  | {} | {} | {} |".format(A1, A2, A3))
+    print("b  | {} | {} | {} |".format(B1, B2, B3))
+    print("c  | {} | {} | {} |".format(C1, C2, C3))
+
+    if (rodada == 1):
+        g = random.randint(0, 1)
+        if (g == 0):
+            jogadorDaVez = "❌"
+        else:
+            jogadorDaVez = "⭕"
+    elif (rodada > 1):
+        if (jogadorDaVez == "❌"):
+            jogadorDaVez = "⭕"
+        else:
+            jogadorDaVez = "❌"
+    print("JOGADOR DA VEZ: {}".format(jogadorDaVez))
+
+    opçõesValidas = False
+    while (opçõesValidas == False):
+        try:
+            l = (input("Em qual linha (A, B OU C) você deseja jogar?\n-> "))
+            l = l.lower()
+            c = (int(input("Em qual coluna (1, 2 OU 3) você deseja jogar?\n-> ")))
+            if ((l == "a" or l == "b" or l == "c") and (1 <= c <= 3)):
+                if (l == "a") and (c == 1):
+                    if (A1 == "a1"):
+                        A1 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "a") and (c == 2)):
+                    if (A2 == "a2"):
+                        A2 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "a") and (c == 3)):
+                    if (A3 == "a3"):
+                        A3 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "b") and (c == 1)):
+                    if (B1 == "b1"):
+                        B1 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "b") and (c == 2)):
+                    if (B2 == "b2"):
+                        B2 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "b") and (c == 3)):
+                    if (B3 == "b3"):
+                        B3 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "c") and (c == 1)):
+                    if (C1 == "c1"):
+                        C1 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "c") and (c == 2)):
+                    if (C2 == "c2"):
+                        C2 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+                elif ((l == "c") and (c == 3)):
+                    if (C3 == "c3"):
+                        C3 = jogadorDaVez
+                        rodada += 1
+                        opçõesValidas = True
+                    else:
+                        print("POSIÇÃO JÁ OCUPADA!!!")
+            else:
+                print("VOCÊ DEVE DIGITAR:\nLINHA = 'a', 'b', ou 'c'\nCOLUNAS = 1, 2 OU 3")
+        except ValueError:
+            print("DIGITE APENAS NÚMEROS!!!")
+            
+    if (A1 == A2 == A3):
+        jogoFinalizado = True
+    elif (B1 == B2 == B3):
+        jogoFinalizado = True
+    elif (C1 == C2 == C3):
+        jogoFinalizado = True
+    elif (A1 == B1 == C1):
+        jogoFinalizado = True
+    elif (A2 == B2 == C2):
+        jogoFinalizado = True
+    elif (A3 == B3 == C3):
+        jogoFinalizado = True
+    elif (A1 == B2 == C3):
+        jogoFinalizado = True
+    elif (A3 == B2 == C1):
+        jogoFinalizado = True
+    else:
+        jogoFinalizado = False
+
+    if (jogoFinalizado == True):
+        print("Após {} rodadas, nós temos um vencedor!!!\nPARÁBENS, JOGADOR {}, VOCÊ FOI O GRANDE VENCEDOR DESSA PARTIDA!!!".format((rodada - 1), jogadorDaVez))
+        print("     1    2    3 ")
+        print("a  | {} | {} | {} |".format(A1, A2, A3))
+        print("b  | {} | {} | {} |".format(B1, B2, B3))
+        print("c  | {} | {} | {} |".format(C1, C2, C3))
+    elif (rodada == 10):
+        print("IHHHH!!! DEU 'VELHA', PORTANTO, NÃO TEMOS VENCEDORES, O JOGO TERMINOU EMPATADO!!!")
+        print("     1    2    3 ")
+        print("a  | {} | {} | {} |".format(A1, A2, A3))
+        print("b  | {} | {} | {} |".format(B1, B2, B3))
+        print("c  | {} | {} | {} |".format(C1, C2, C3))
