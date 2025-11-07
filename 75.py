b16810bc5185926b9dc5648251934cb9056df382
ricardocassio07 <ricardocassio271@gmail.com>
2025-11-07 11:44:00 -0300

    Aprendendo Python

--- 75.py
+++ 75.py
@@ -11,36 +11,86 @@ Requisitos técnicos:
 import random
 
 letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-print(len(letras))
+# print(len(letras))
 numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-print(len(numeros))
+# print(len(numeros))
 caracteres_especiais = ["@", "$", "#", "&", "/", "!", "?"]
+
+def senha(numero_de_digitos): 
+    verificado = False
+    while (verificado == False):
+        estilo_da_senha = (int(input(
+            "\nVOCÊ DESEJA UMA SENHA:"\
+            "\nDIGITE:\n"\
+            "1- FORTE\n"\
+            "2- INTERMEDIÁRIA\n"\
+            "3- FRACA\n"\
+            "-> "
+        )))
+        if (1 <= estilo_da_senha <= 3):
+            verificado = True
+        else:
+            print("-> OPÇÃO INVÁLIDA!!! <-")
+    
+    senha = ""
+    if (estilo_da_senha == 1):
+        for i in range(numero_de_digitos):
+            opcao = random.randint(1, 3)
+            if (opcao == 1):
+                indexLetra = random.randint(0, 25)
+                letra = letras[indexLetra]
+                # Determinar se a letra vai ser maiúscula ou minúscula. Caso estilo for igual a 1, troca o estilo para maiúsculo:
+                estilo = random.randint(0, 1)
+                if (estilo == 1):
+                    letra = letra.upper()
+                senha += letra
+            elif (opcao == 2):
+                indexNumero = random.randint(0, 9)
+                senha += numeros[indexNumero]
+            elif (opcao == 3):
+                indexCaractereEspecial = random.randint(0, 6)
+                senha += caracteres_especiais[indexCaractereEspecial]
+    elif (estilo_da_senha == 2):
+        for i in range(numero_de_digitos):
+            opcao = random.randint(1, 2)
+            if (opcao == 1):
+                indexLetra = random.randint(0, 25)
+                letra = letras[indexLetra]
+                # Determinar se a letra vai ser maiúscula ou minúscula. Caso estilo for igual a 1, troca o estilo para maiúsculo:
+                estilo = random.randint(0, 1)
+                if (estilo == 1):
+                    letra = letra.upper()
+                senha += letra
+            elif (opcao == 2):
+                indexNumero = random.randint(0, 9)
+                senha += numeros[indexNumero]
+    elif (estilo_da_senha == 3):
+        for i in range(numero_de_digitos):
+            indexLetra = random.randint(0, 25)
+            letra = letras[indexLetra]
+            # Determinar se a letra vai ser maiúscula ou minúscula. Caso estilo for igual a 1, troca o estilo para maiúsculo:
+            estilo = random.randint(0, 1)
+            if (estilo == 1):
+                letra = letra.upper()
+            senha += letra
+            
+    senha = senha.strip()
+    senha = ("\nSENHA: " + senha)
+    return senha
 
 continuar = True
 while (continuar == True):
     try:
-        qtdeDigitos = (int(input("QUANTOS DÍGITOS VOCÊ DESEJA QUE A SENHA CONTENHA?\n-> ")))
         verificado = False
         while (verificado == False):
-            estilo_da_senha = (int(input(
-                "\nVOCÊ DESEJA UMA SENHA:"\
-                "\nDIGITE:\n"\
-                "1- FORTE\n"\
-                "2- INTERMEDIÁRIA\n"\
-                "3- FRACA\n"\
-                "-> "
-            )))
-            if (1 <= estilo_da_senha <= 3):
+            qtdeDigitos = (int(input("QUANTOS DÍGITOS VOCÊ DESEJA QUE A SENHA CONTENHA?\n-> ")))
+            if (qtdeDigitos > 0):
                 verificado = True
             else:
-                print("-> OPÇÃO INVÁLIDA!!! <-")
-        
-        senha: str 
-        for i in range(qtdeDigitos):
-            opcao = random.radiant(1, 3)
-            if (opcao == 1)
-            elif (opcao == )
+                print("-> A SENHA DEVE TER PELO MENOS 1 DÍGITO!!! <-")
 
+        resultado = senha(qtdeDigitos)
+        print(resultado)
 
         valido = False
         while (valido == False):
