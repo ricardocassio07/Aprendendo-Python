cc78e715b5e956640e6b614fe0821a87c51a45ad
ricardocassio07 <ricardocassio271@gmail.com>
2025-11-07 11:44:32 -0300

    Aprendendo Python

---
+++ 76.py
@@ -0,0 +1,10 @@
+'''
+    5: Desenvolva uma calculadora que calcule impostos progressivos baseados em
+    diferentes faixas de renda, com simulações de cenários e comparações anuais.
+Requisitos técnicos:
+    - Use range para iterar pelas faixas de imposto
+    - Implemente try-except para validação de valores monetários
+    - Use if-elif-else para aplicar diferentes alíquotas
+    - Crie função para cálculos de cada faixa
+    - Use while para simulações múltiplas 
+'''
